916982286ea79ff147daae681fca758188a630b1
北名 <zyk2003xxx@163.com>
2021-06-12 09:13:36 +0800

    feat: tree util

--- src/function/demo.tsx
+++ src/function/demo.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { debounce } from './index'
 import './bind'
 
 
 const fff = {
     f : 1,
-    gg() {
+    gg() { 
         debugger
         alert(this.f)
     }
@@ -25,9 +25,21 @@ function fun(a){
 }
 
 
+let fffr = '11e'
+
+const aaFFF = ()=> {
+    const [tt] = useState('tt')
+    return tt
+}
 export default () => {
-    return <div>
+    const  [a,aa1] = useState(2)
+    const  b = aaFFF() 
+    // const  [c,bb13] = useState(3)
+return <div><h1>{a},, {b}</h1>
         <button onClick={debounce(aa.a1.bind(aa))}>debounce</button>
         <button onClick={fun.MyBind(obj, 'ttt')}>bind</button>
+        <button onClick={()=>  { fffr = 'yyyy' , aa1(+new Date()) }}>dd</button>
+        <button onClick={()=> { aa1(+ new Date())}}>rrr</button>
+        <input onChange={(e) => {  setTimeout(()=> alert(e.target.value))  }} ></input>
         </div>
 }
--- src/vue-temple/index.tsx
+++ src/vue-temple/index.tsx
@@ -1,20 +1,42 @@
-import React, { isValidElement } from 'react';
+import React, { isValidElement, useState } from 'react';
+import { Input, Row, Col } from 'antd'
 import { compile } from './compile';
 
-const result = compile(`
-  <View id="test" :name="'111'" @click="alert()">
-    <Form>
-      <p>This is my vue render test</p>
-    </Form>
-    <Row>my name is {{myName}}</Row>
-  </View>`);
+const tpl = `
+<View id="test" :name="'111'" @click="alert()">
+  <Form>
+    <p>This is my vue render test</p>
+  </Form>
+  <Row>my name is {{myName}}</Row>
+</View>`
 
-console.log(result);
+const result = compile(tpl);
+
+const makeFunction = () => {
+  return new Function('_c',)
+}
+
+const tryCompile = (c: string) => {
+  try {
+    return compile(c)
+  } catch (ex) {
+    return ex.message
+  }
+}
+
 export default () => {
-  return <div>{renderJson(result)}</div>;
+  const [code, setCode] = useState(tpl)
+  return <div>
+    <Row>
+      <Col span={12}><Input.TextArea value={code} onChange={(e) => setCode(e.target.value)} rows={15} /></Col>
+      <Col span={12}>{renderJson(tryCompile(code))}</Col>
+    </Row>
+   
+   
+  </div>;
 };
 
-export const json = (obj, replacer?, space?) => {
+export const json = (obj: any, replacer?: any, space?: any) => {
   const _replacer = replacer || null;
 
   const _space = space || 2;
@@ -46,9 +68,7 @@ const handleCircular = () => {
 
 export const renderJson = (value: any, replacer?: any, space?: any) => {
   const _replacer = replacer || handleCircular();
-
   const _json = JSON.stringify(value, _replacer, space);
-
   const _res = JSON.parse(_json);
 
   return (
